f12dd38409fa6c0561c954d5fe9bb375b8019867
Tijani891 <ahmedayobami91@gmail.com>
2024-01-21 01:28:59 +0100

    Rolling update

--- Terraform-Files/main.tf
+++ Terraform-Files/main.tf
@@ -65,22 +65,14 @@ module "eks" {
     one = {
       name = "node-group-1"
 
-      instance_types = ["t2.micro"]
+      instance_types = ["t2.medium"]
 
       min_size     = 1
       max_size     = 3
       desired_size = 1
     }
 
-    two = {
-      name = "node-group-2"
 
-      instance_types = ["t2.micro"]
-
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
-    }
   }
 }
 
